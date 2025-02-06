14d995f3af934cf9708713d54d247efc240f1d6a
ARTEMII ARTYUSHCHENKO <artiuschenko.a@gmail.com>
2025-02-06 14:12:31 +0100

    initial commit

---
+++ number_guessing.py
@@ -0,0 +1,59 @@
+import sys
+import random
+
+def select_difficulty():
+    print('''Please select the difficulty level:
+1. Easy (10 chances)
+2. Medium (5 chances)
+3. Hard (3 chances)\n''')    
+
+    while True: 
+        choise = int(input('Enter your choise: 1, 2 or 3: ').strip())
+        match choise:
+            case 1: return 10
+            case 2: return 5
+            case 3: return 3
+            case default: print('Invalid input.')
+
+def set_random_number():
+    return random.randint(1,100)
+
+def prompt_guess(prompt, input_func = input):
+    while True: 
+        try:
+            guess = int(input_func(prompt).strip())
+            if guess < 1 or guess > 100:
+                print('Invalid input.')
+                continue
+            return guess
+        except ValueError:
+            print('Invalid input')
+
+def evaluate_input(number, difficulty):
+    count = 1
+    while count <= difficulty:
+        guess = prompt_guess('Guess your number: ')
+        if guess < number:
+            print('Incorrect! The number is greater than %d.\n' % guess)
+            count = count + 1
+        elif guess > number:
+            print('Incorrect! The number is less than %d.\n' % guess)
+            count = count + 1
+        else :
+            print('Congratulations! You guessed the correct number in %d attempts.' % count)
+            sys.exit()
+    if count > difficulty:
+        print('You lost.')
+        sys.exit()
+
+
+def main():
+    difficulty = select_difficulty()
+    print(difficulty)
+    number = set_random_number()
+    evaluate_input(number, difficulty)
+    
+    
+
+if __name__ == '__main__':
+    main()
